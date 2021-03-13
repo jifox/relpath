8969d1e0906eb28d70f41482d1465b7985fd03c5
Josef Fuchs <josef.fuchs@pankl.com>
2021-03-13 21:23:46 +0100

    Add github actions

--- relpath/__init__.py
+++ relpath/__init__.py
@@ -1,21 +1,22 @@
+"""Get relative-path even when it is not in subdirectory tree."""
 import os
 from pathlib import Path
 from typing import Union
 
 
-def relative_path(base_path:Union[str,Path], relative_path:Union[str,Path]) -> str:
-    """Returns the relative path from base path to relative_path
-    even when relative_path is not a subdirectory of base_path.
+def relative_path(base_path: Union[str, Path], rel_path: Union[str, Path]) -> str:
+    """Returns the relative path from base path to rel_path
+    even when rel_path is not a subdirectory of base_path.
 
     Args:
         base_path (str|Path): Base for relative path
-        relative_path (str|Path): File or Directory that the relative path points to.
+        rel_path (str|Path): File or Directory that the relative path points to.
 
     Returns:
-        str: relative path to access 'relative_path' from 'base_path'
+        str: relative path to access 'rel_path' from 'base_path'
     """
     base = Path(base_path).absolute()
-    rel = Path(relative_path).absolute()
+    rel = Path(rel_path).absolute()
 
     if not base.is_dir():
         base = base.parent
@@ -23,7 +24,7 @@ def relative_path(base_path:Union[str,Path], relative_path:Union[str,Path]) -> s
     common = ""
     idx = 0
     while (idx < min(len(base.parts), len(rel.parts))) and (
-          str(base.parts[idx]) == str(rel.parts[idx])
+        str(base.parts[idx]) == str(rel.parts[idx])
     ):
         if len(common) > 0 and (common[-1] != os.sep):
             common += os.sep
@@ -41,5 +42,5 @@ def relative_path(base_path:Union[str,Path], relative_path:Union[str,Path]) -> s
         ofs = 1
     elif len(res) == 0:
         ofs = 1
-    res += str(rel)[len(str(common))+ofs :]
+    res += str(rel)[len(str(common)) + ofs :]
     return res
